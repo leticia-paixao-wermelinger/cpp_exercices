73ac7829c14d055d2d20eb431af0b75f177dbbe7
Letícia Paixão <leticiapwermelinger@gmail.com>
2025-01-03 02:05:43 -0300

    Validação de existência prévia de arquivo .replace

--- 1.4.myOwnSed/main.cpp
+++ 1.4.myOwnSed/main.cpp
@@ -1,9 +1,17 @@
 #include "File.hpp"
+#include <filesystem>
+
+int	fileExists(const char *filename);
 
 int main ()
 {
 	File	objFile;
 	const char	*filename = objFile.getNewFilename();
+	if (fileExists(filename) == true)
+	{
+		std::cout << "Arquivo já existe" << std::endl;
+		return 0;
+	}
 	std::ofstream	myNewFile(filename);
 
 	if (myNewFile.is_open())
@@ -18,4 +26,16 @@ int main ()
 	else
 		std::cout << "Erro ao criar o arquivo" << filename << std::endl;
 	return (0);
+}
+
+int	fileExists(const char *filename)
+{
+	std::ifstream	fs(filename);
+
+	if (fs.is_open())
+	{
+		fs.close();
+		return true;
+	}
+	return false;
 }
