1c0669432c1507adcfbdb0886ff3532bccc31626
lpaixao- <leticiapwermelinger@gmail.com>
2025-05-05 18:54:31 -0300

    3.0 ready to start

---
+++ 3.0.ClapTrap/ClapTrap.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaixao- <lpaixao-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 17:09:47 by lpaixao-          #+#    #+#             */
+/*   Updated: 2025/05/05 17:09:48 by lpaixao-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+    std::string _name;
+    unsigned int _hitPoints; // iniciado em 10
+    unsigned int _energyPoints; // iniciado em 10
+    unsigned int _attackDamage; // iniciado em 0
+    void        setName( const std::string& name );
+    void        setHitPoints( void );
+    void        setEnergyPoints( void );
+    void        setAttackDamage( void );
+    std::string getName( void ) const;
+    int         getHitPoints( void ) const;
+    int         getEnergyPoints( void ) const;
+    int         getAttackDamage( void ) const;
+public:
+    ClapTrap(); // construtor padrão
+    ClapTrap(const std::string& name); // construtor com nome
+    ClapTrap(const ClapTrap& other); // construtor de cópia
+    ClapTrap& operator=(const ClapTrap& other); // operador de atribuição
+    ~ClapTrap(); // destrutor
+    void    attack(const std::string& target);
+    void    takeDamage(unsigned int amount);
+    void    beRepaired(unsigned int amount);
+};
