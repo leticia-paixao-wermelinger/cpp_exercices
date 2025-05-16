fe3c4857b9679ff6d69a3dc1433ba82cb0de3376
Letícia Paixão <leticiapwermelinger@gmail.com>
2025-05-15 22:16:46 -0300

    Files created

---
+++ 4.0.Polymorphism/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaixao- <lpaixao-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 22:07:26 by lpaixao-          #+#    #+#             */
+/*   Updated: 2025/05/15 22:08:28 by lpaixao-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include <iostream>
+
+int main() // subject main
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	return 0;
+}
