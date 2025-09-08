5343295be122a3f28c7e089c409d8f5dfc13a5cd
Letícia Paixão <leticiapwermelinger@gmail.com>
2025-09-07 22:57:18 -0300

    header is almost ready (lacking the addRange parameters)

--- 08.1.Span/Span.hpp
+++ 08.1.Span/Span.hpp
@@ -12,13 +12,21 @@
 
 #pragma once
 
+#include <vector>
+
 class Span
 {
-private:
-	/* data */
-public:
-	Span();
-	Span(const Span &other);
-	Span &operator=(const Span &other);
-	~Span();
+	private:
+		unsigned int _N;
+		std::vector<int> _numbers;
+		Span();
+	public:
+		Span(unsigned int N);
+		Span(const Span &other);
+		Span &operator=(const Span &other);
+		~Span();
+		void	addNumber(int number); // in case of error, throw an exception
+		void	addRange(/*...*/); // adds multiple numbers in a single call
+		int		shortestSpan() const; // in case of error, throw an exception
+		int		longestSpan() const; // in case of error, throw an exception
 };
--- 08.1.Span/main.cpp
+++ 08.1.Span/main.cpp
@@ -16,7 +16,18 @@
 #include <deque>
 #include <iostream>
 
+// test code with at least 10000 numbers (preferably more)
+
+// subject main
 int	main()
 {
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
 	return 0;
 }
