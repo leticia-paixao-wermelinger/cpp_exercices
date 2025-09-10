267ef6b82d035754d769ad94c6a7e263657067bf
Letícia Paixão <leticiapwermelinger@gmail.com>
2025-09-09 22:36:08 -0300

    mutant stack pronto - falta melhorar e realizar mais testes!

--- 08.2.MutantStack/MutantStack.hpp
+++ 08.2.MutantStack/MutantStack.hpp
@@ -16,20 +16,32 @@
 #include <iterator>
 #include <iostream>
 
+/*
+A stack always has a deque (double-ended queue) as its underlying container.
+This means that it is possible to access easily the first and last elements
+of the stack using the deque's member functions. The same applies to the
+iterator of the deque.
+*/
+
 template <typename T>
 class MutantStack : public std::stack<T>
 {
 	private:
 	public:
-		MutantStack();
-		MutantStack(const MutantStack &other);
-		MutantStack &operator=(const MutantStack &other);
-		~MutantStack();
+		MutantStack() : std::stack<T>() {};
+		MutantStack(const MutantStack &other) : std::stack<T>(other) {};
+		MutantStack &operator=(MutantStack const &rhs)
+		{
+			std::stack<T>::operator=(rhs);
+			return *this;
+		}
+		~MutantStack() {};
 
-		// iterator (?)
+		// acessando o iterator de dentro do container deque que está dentro da stack. Agr, para chamar ele, basta digitar "iterator" e pronto.
+		typedef typename std::stack<T>::container_type::iterator iterator;
 
-		// begin()
-		// end()
+		iterator begin(); // Primeiro inserido = fundo da pilha
+		iterator end(); // último inserido = topo da pilha
 };
 
 #include "MutantStack.tpp"
