ae6fbcdec9e79dd6a9783e9ccc8ef3d33bf5e5aa
Letícia Paixão <leticiapwermelinger@gmail.com>
2025-09-11 22:14:12 -0300

    MutantStack with more tests

--- 08.2.MutantStack/main.cpp
+++ 08.2.MutantStack/main.cpp
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
+#include <typeinfo>
 
 // subject main
 int	main()
@@ -37,5 +39,41 @@ int	main()
         ++it;
     }
     std::stack<int> s(mstack);
+    std::cout << "size(mstack) = " << mstack.size() << std::endl;
+    std::cout << "size(s)      = " << s.size() << std::endl;
+    mstack.pop();
+    std::cout << "Depois de pop() em mstack:" << std::endl;
+    std::cout << "size(mstack) = " << mstack.size() << std::endl;
+    std::cout << "size(s)      = " << s.size() << std::endl;
+    std::cout << "mstack type = " << typeid(mstack).name() << std::endl;
+    std::cout << "s type      = " << typeid(s).name() << std::endl;
     return 0;
 }
+/*
+// subject main with list
+int	main()
+{
+    std::list<int> lstack;
+    lstack.push_back(5);
+    lstack.push_back(17);
+    std::cout << lstack.back() << std::endl;
+    lstack.pop_back();
+    std::cout << lstack.size() << std::endl;
+    lstack.push_back(3);
+    lstack.push_back(5);
+    lstack.push_back(737);
+    //[...]
+    lstack.push_back(0);
+    std::list<int>::iterator it = lstack.begin();
+    std::list<int>::iterator ite = lstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    //std::deque<int> deque_stack(lstack.begin(), lstack.end());
+    //std::stack<int> s(deque_stack);
+    return 0;
+}*/
